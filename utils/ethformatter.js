290e62f45cb70ad917fd5b8610a24af756ca4faf
TonyHu <huzhengkui@126.com>
2018-04-14 12:40:17 +0800

    instead of ETH as OKC

--- utils/ethformatter.js
+++ utils/ethformatter.js
@@ -1,10 +1,10 @@
 var BigNumber = require('bignumber.js');
 
-var Ether     = new BigNumber(10e+17);
+var OKC     = new BigNumber(10e+17);
 
 function formatAmount(amount) {
   var ret = new BigNumber(amount.toString());
   
-  return ret.dividedBy(Ether) + " ETH";
+  return ret.dividedBy(OKC) + " OKC";
 }
 module.exports = formatAmount;
