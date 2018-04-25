8b2b48800c37f5230d9f7de5ecfe2a306b8b7272
TonyHu <huzhengkui@126.com>
2018-04-25 17:43:21 +0800

    new update

--- routes/tx.js
+++ routes/tx.js
@@ -92,94 +92,60 @@ router.get('/:tx', function(req, res, next) {
       web3.okc.getTransactionReceipt(result.hash, function(err, receipt) {
         callback(err, result, receipt);
       });
-    }, function(tx, receipt, callback) {  
-    	var blk = tx.blockNumber;
-    	if(tx.blockNumber < 10)
-    	{
-    		blk = tx.blockNumber;
-    	}
-    	else
-    	{
-    		blk = 10;
-    	}
-    	
-      
-      var traces;
-
-    	for(;blk > 0;blk--)
-    	{
-    	   web3.okc.getBlock(blk,function(err, blk_result) {
-			   if(err)
-			   {
-				 console.log("penging error");
-			   }
-			   else
-			   {	
-				   blk_result.transactions.forEach(function(txHash){
-						   web3.okc.getTransactionReceipt(txHash, function(err, tx_result) 
-						   {
-							    if(tx_result.from == tx.from || tx_result.from == tx.to || tx_result.from == tx.to || tx_result.to == tx.from)
-							    	{
-							    		traces.push(tx_result);
-							    	}
-						    });
-					   });
-			   }
-	        });
-    	}
-    	//}
-//      web3.trace.transaction(tx.hash, function(err, traces) {
-          //callback(err, tx, receipt, traces);
-//      });
-        callback(null, tx, receipt, traces);
-    }, function(tx, receipt, traces, callback) {
-
-      db.get(tx.to, function(err,  value) {
-        callback(null, tx, receipt, traces,value);
-      });
     }
-  ], function(err, tx, receipt, traces, source) {
+  ], function(err, tx, receipt) {
+
     if (err) {
       return next(err);
     }
-   
-
-     var callsource;
-
-    // Try to match the tx to a solidity function call if the contract source is available
-    if (source) {
-      tx.source = JSON.parse(source);
-
-      try {
-        var jsonAbi = JSON.parse(tx.source.abi);
-
-        abiDecoder.addABI(jsonAbi);
-        tx.logs = abiDecoder.decodeLogs(receipt.logs);
-        tx.callInfo = abiDecoder.decodeMethod(tx.input);
-      } catch (e) {
-        console.log("Error parsing ABI:", tx.source.abi, e);
-      }
+ 
+    var trace_result = {
+      address:"",
     }
+
+    var trace_action = {
+          author:"",
+          from:"",
+          to:"",
+          value:0
+        }
+
+
+    var trace = {
+        type  :"unknow",
+        blockNumber:0,
+        blockHash:"0x00000000000",
+        transactionHash:"0x00000000000",
+        err:0,
+        result:trace_result,
+        action:trace_action
+    };
+
+    trace.type = "call";
+    trace.action.from = tx.from;
+
+    console.log(tx.to);
+    if(tx.to == null){
+      trace.action.to = receipt.contractAddress;
+      tx.contractAddress =  receipt.contractAddress;
+    }
+    else{
+      trace.action.to = tx.to;
+    }
+    
+    trace.action.value = tx.value;
+    trace.transactionHash = tx.hash;    
+    trace.blockNumber = tx.blockNumber;
+    trace.err = receipt.gasUsed;
+
     tx.traces = [];
     tx.failed = false;
-    tx.gasUsed = 0;
-    if (traces != null) {
-    traces.forEach(function(trace) {
-        tx.traces.push(trace);
-        if (trace.error) {
-          tx.failed = true;
-          tx.error = trace.error;
-        }
-        if (trace.gasUsed) {
-          tx.gasUsed += parseInt(trace.gasUsed, 16);
-        }
-      });
-    }
-    // console.log(tx.traces);    
-    res.render('tx', { tx: tx});
-    res.render('traces', { traces: traces});
-  });
-  
+    tx.gasUsed = receipt.gasUsed;
+    tx.traces.push(trace);
+   
+    res.render('tx', { tx: tx});  
+});
+
 });
 
 router.get('/raw/:tx', function(req, res, next) {
@@ -193,20 +159,15 @@ router.get('/raw/:tx', function(req, res, next) {
       web3.okc.getTransaction(req.params.tx, function(err, result) {
         callback(err, result);
       });
-//    }, function(result, callback) {
-//      web3.trace.replayTransaction(result.hash, ["trace", "stateDiff", "vmTrace"], function(err, traces) {
-//        callback(err, result, traces);
-//      });
     }
   ], function(err, tx) {
     if (err) {
       return next(err);
     }
     
-  //  tx.traces = traces;
+    tx.traces = [];
 
     res.render('tx_raw', { tx: tx });
-    res.render('traces', { traces: traces});
   });
 });
 
